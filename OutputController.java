a69fbdbb7ab0975701fac56892d4d2c47ba7991d
SitiHafijahZahraSiregar <92381404+SitiHafijahZahraSiregar@users.noreply.github.com>
2022-12-07 11:00:49 +0700

    Create OutputController.java

---
+++ OutputController.java
@@ -0,0 +1,42 @@
+package praktikum15_ara;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Praktikan
+ */
+public class OutputController {
+
+    @FXML
+    private Button btnNew;
+
+    @FXML
+    private Label lblAlamat;
+
+    @FXML
+    private Label lblNama;
+
+    @FXML
+    private Label lblNpm;
+
+    @FXML
+    void openForm(ActionEvent event) throws IOException {
+
+    }
+    
+}
