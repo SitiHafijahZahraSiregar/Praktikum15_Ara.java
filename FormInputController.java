9abc6b210be534265bebe5fe40b9cf77032d4e6b
SitiHafijahZahraSiregar <92381404+SitiHafijahZahraSiregar@users.noreply.github.com>
2022-12-07 10:59:35 +0700

    Create FormInputController.java

---
+++ FormInputController.java
@@ -0,0 +1,42 @@
+package praktikum15_ara;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Praktikan
+ */
+public class FormInputController {
+
+     @FXML
+    private TextField FildAlamat;
+
+    @FXML
+    private Button btnTambah;
+
+    @FXML
+    private TextField fildNama;
+
+    @FXML
+    private TextField fildNpm;
+
+    @FXML
+    void sendData(ActionEvent event) throws IOException{
+
+    }
+
+}
